3ab4ae47ead5875ca9f2539108dd037f969636c1
LuizOtavioDC <luiz.otavio.dc@hotmail.com>
2022-01-21 08:47:01 -0300

    created project

---
+++ backend/src/test/java/com/luiz/clients/clients/ClientsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.luiz.clients.clients;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClientsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
